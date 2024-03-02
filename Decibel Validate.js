4f0784170afa32431e80a3403b4326ea6defea9e
Christian Dela Rosa <89810116+chrisngem@users.noreply.github.com>
2024-03-02 15:51:37 -0800

    Add files via upload

---
+++ Decibel Validate.js
@@ -0,0 +1,9 @@
+// Decibel Validate.js
+
+function checkSearch() {
+	let searchTerm = document.getElementById("#search_term");
+
+	if (searchTerm === '') {
+		console.log "Please Enter a Search Term..."
+	}
+}
